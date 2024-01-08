2cfda725eeb62bb7dfe2e6e2db307dc6e87f9f0c
2Heeae <101415696+2Heeae@users.noreply.github.com>
2024-01-08 16:15:36 +0900

    [To-Do]리스트에 새로운 todo 추가
    새로운 todo를 리스트에 추가하고, 자동으로 새로고침 하는 form특성을 막기위해 submit.prevent사용

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,56 @@
 <template>
-
-  <input 
-    type="text" 
-    v-model="name"
-    >
-  <button 
-    class="btn btn-primary" 
-    @click="onSubmit">
-      Click
-  </button>
+<div class="container">
+  <h1>To-Do List</h1> 
+  <!-- 
+    form은 submit이 되면 reloading함... 
+    이걸 방지하기 위해 onSubmit함수에 e.preventDefault(); 선언
+    그치만 vue에서는 @submit.prevent 쓰면 똑같은 효과!
+   -->
+  <form 
+    @submit.prevent="onSubmit"
+    class="d-flex"
+  >
+    <div class="flex-grow-1">
+      <input 
+        class="form-control"
+        type="text" 
+        v-model="todo"
+        placeholder="Type new to-do"
+      >
+    </div>
+    <div>
+      <button 
+        class="btn btn-primary" 
+        type="submit"
+      >
+        Add
+      </button>
+    </div>
+  </form>
+  {{ todos }}
+</div>
 </template>
 
 <script>
 import {ref} from 'vue';
 export default{
   setup() {
-    const name = ref('hello');
+    const todo = ref('');
+    const todos = ref([]);
 
-
-    const onSubmit = () => {
-      console.log(name.value);
+    const onSubmit = (e) => {   
+      e.preventDefault();
+      console.log(Date.now());
+       //id는 유니크해야하니까 현재시간으로 설정
+      todos.value.push({
+        id: Date.now(),
+        subject: todo.value
+      });
     };
 
-    // const updateName = (e) => {
-    //   name.value = e.target.value;
-    //   // console.log(e.target.value);
-    // };
-
     return{
-      name,
+      todo,
+      todos,
       onSubmit
     }
   }
