36ea78a78e29daf2f018dac2154770d7c5bc9631
2Heeae <101415696+2Heeae@users.noreply.github.com>
2024-01-09 15:08:52 +0900

    [To-Do] async, await 사용하기

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="container">
-    <h1>To-Do List</h1>
+    <h1 class="display-4 text-center p-3">To-Do List</h1>
     <input 
           class="form-control"
           type="text" 
           v-model="searchText"
-          placeholder="검색"
+          placeholder="빠른 검색"
     > 
     <hr />
     <TodoSimpleFormVue @add-todo="addTodo" />
     <div style="color:red">{{ error }}</div>
 
-    <div v-if="!filteredTodos.length">
+    <div 
+      class="p-3"
+      v-if="!filteredTodos.length"
+    >
       내용이 없습니다.
     </div>
     <TodoListVue 
@@ -38,21 +41,20 @@ export default{
     const todos = ref([]);
     const error = ref('');
 
-    const addTodo = (todo) => {
+    const addTodo = async (todo) => {
       // DB에 todo를 저장
       error.value = '';
-      axios.post('http://localhost:3000/todos', {
+      try{
+        const res = await axios.post('http://localhost:3000/todos', {
         //id는 1부터 차례대로 저장됨
         subject: todo.subject,
         completed: todo.completed
-      }).then(res => {
-        //응답이 오면 리스트에 추가하도록..
-        console.log(res);
+        });
         todos.value.push(res.data);
-      }).catch(err => {
+      } catch (err) {
         console.log(err);
         error.value = '에러가 발생하였습니다.';
-      });
+      }
     };
 
     const toggleTodo = (index) => {
